333f1a3562a8d5536bfbcf8cc03bfac271bc5393
Da Kim <daeunkim127@gmail.com>
2022-07-10 01:43:11 -0400

    data.js added

---
+++ utils/data.js
@@ -0,0 +1,36 @@
+const users = [
+    {
+      email: "daeun@gmail.com",
+      username: "daeun",
+      thoughts: [],
+      friends: [],
+    },
+    {
+      email: "daisy@gmail.com",
+      username: "daisy",
+      thoughts: [],
+      friends: [],
+    },
+    {
+      email: "robert@gmail.com",
+      username: "robert",
+      thoughts: [],
+      friends: [],
+    }
+
+  ];
+  
+  const thoughts = [
+    "Nice",
+    "Very Good",
+    "Bad",
+
+  ];
+  
+  const reactions = [
+    "!!!",
+    "??",
+    "......"
+  ];
+  
+  module.exports = { users, thoughts, reactions };
